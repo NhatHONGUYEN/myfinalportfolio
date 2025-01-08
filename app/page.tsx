3681a50ed6a782fdb72a94f5eca1aed1b494b123
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-08 15:57:44 +0100

    💄 Home : add blur animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
+import { FC } from 'react';
+import { InView } from '@/components/ui/in-view';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 
-export default function Home() {
+// Common animation variants
+const animationVariants = {
+  hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
+  visible: { opacity: 1, y: 0, filter: 'blur(0px)' },
+};
+
+// Common transition configuration
+const transitionConfig = { duration: 0.3, ease: 'easeInOut' };
+
+const Home: FC = () => {
   return (
     <>
       <Hero />
-      <Projects />
-      <Contact />
+
+      <InView variants={animationVariants} transition={transitionConfig}>
+        <Projects />
+      </InView>
+      <InView variants={animationVariants} transition={transitionConfig}>
+        <Contact />
+      </InView>
       <Footer />
     </>
   );
-}
+};
+
+export default Home;
