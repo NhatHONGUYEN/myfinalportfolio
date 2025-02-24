56446fe6fabaae66442eeb7cde126da270aced5a
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-24 12:17:55 +0100

    ✨ add scrolltotop

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { ThemeProvider } from '@/components/theme-provider';
 import ParticlesBackGround from '@/components/ParticlesBackGround';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
 
 const workSans = Work_Sans({
   subsets: ['latin'],
@@ -41,6 +42,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
+            <ScrollToTopButton />
           </div>
         </ThemeProvider>
       </body>
---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from './ui/button';
+import { useEffect, useState } from 'react';
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const label = 'Go to top';
+  return (
+    <div className="fixed bottom-4 right-4">
+      <Button onClick={scrollToTop} size="lg" className="group flex-col">
+        {label}
+      </Button>
+    </div>
+  );
+}
